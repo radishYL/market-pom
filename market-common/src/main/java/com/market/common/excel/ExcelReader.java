1f862e8c3f5bffa2b96c879a8830732400b80c70
Alex <Alex@192.168.1.104>
2018-08-27 20:45:34 +0800

    test

---
+++ market-common/src/main/java/com/market/common/excel/ExcelReader.java
@@ -0,0 +1,19 @@
+package com.market.common.excel;
+
+/**
+ * excel读取工具
+ * @author Alex
+ *
+ * @param <T>
+ */
+public class ExcelReader<T> {
+
+
+	public T readExcel(String excelPath) throws Exception{
+//		 Type type = ((ParameterizedType)this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		
+		return null;
+	}
+	
+	
+}
---
+++ market-common/src/test/java/com/market/test/ReflectTest.java
@@ -0,0 +1,14 @@
+package com.market.test;
+
+import com.market.common.excel.ExcelReader;
+
+public class ReflectTest {
+
+	public static void main(String[] args) throws Exception {
+		
+		ExcelReader<User> r = new ExcelReader<User>();
+		
+	}
+	
+}
+ 	 
